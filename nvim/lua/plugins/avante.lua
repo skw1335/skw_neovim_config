87a66ff59e74250aad7012c77031a0205a96be16
sam <skw1335@github.com>
2024-11-18 18:24:24 -0500

    my neovim config

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,48 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  opts = {
+    provider = "ollama",
+    vendors = {
+      ---@type AvanteProvider
+      ollama = {
+        ["local"] = true,
+        endpoint = "127.0.0.1:1234/v1",
+        model = "codegemma",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
+  },
+  build = ":AvanteBuild",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
